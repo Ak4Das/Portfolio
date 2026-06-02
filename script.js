5ca92abb701551a0df1019dd09dab20665884f00
Akash Das <akashdas02052@gmail.com>
2026-06-02 12:44:10 +0530

    Add functionality to About Me btn

---
+++ script.js
@@ -0,0 +1,9 @@
+function openAboutMeSection() {
+  const element = document.querySelector("#about-section")
+  element.classList.add("active");
+}
+
+function CloseAboutMeSection() {
+  const element = document.querySelector("#about-section")
+  element.classList.remove("active");
+}
